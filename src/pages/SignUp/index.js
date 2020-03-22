e28add5af9eb8a0314627f9299e053c7c62d75af
roandrebrasil <roandrebrasil@gmail.com>
2020-03-22 13:39:33 -0300

    Refazendo o SignUp com function

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,30 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import api from "../../services/api";
 
 import "./index.css";
 
 export default function Cadastro() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post("/auth/register", {
+      name,
+      email,
+      password
+    });
+    console.log(response.data);
+    const { _id } = response.data;
+
+    localStorage.setItem("user", _id);
+  }
+
   return (
     <div id="containerSign">
       <div id="box">
         <h1>SERVICES</h1>
         <h1>ADS</h1>
         <p className="text1">Crie sua conta</p>
-        <div id="label">
-          <label>Nome</label>
-          <p> Ex: Daniel Belfort </p>
-        </div>
-        <input type="text"></input>
-        <div id="label2">
-          <label>E-mail</label>
-          <p> Será enviado um e-mail de confirmação </p>
-        </div>
-        <input type="email"></input>
-        <div id="label3">
-          <label>Senha</label>
-          <p> Use letras, números e caracteres especiais </p>
-        </div>
-        <input type="password"></input>
-        <button>Cadastrar</button>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Nome *</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Ex: Romulo Brasil"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+
+          <label htmlFor="email">Email *</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Ex: romulo@gmail.com"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <label htmlFor="password">Senha *</label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Use letras, números e caracteres especiais"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+
+          <button type="submit">Cadastrar</button>
+        </form>
 
         <div id="box2">
           <p className="text2">Já possui conta?</p>
@@ -34,4 +68,3 @@ export default function Cadastro() {
     </div>
   );
 }
-
